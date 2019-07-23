97f846c5fc50fdc31bd0d2fdf5d3b04493675913
Matthew Thomas <matthew.thomas@tiani-spirit.com>
2019-07-23 20:57:39 +0100

    pre test

--- RevserseHarm_TankRange.lua
+++ RevserseHarm_TankRange.lua
@@ -1,11 +1,10 @@
 function() 
     -- change tank1 and tank2 local variables to match main tanks of group. 
-    local tank1 = "Daruul"
-    local tank2 = "Ermehdin"	
+    local unit = UnitName("targettarget")
     
-    if UnitExists(tank1) or UnitExists(tank2) then        
+    if unit ~= nil then        
         
-        if(WeakAuras.CheckRange(tank1, 10, "<=") or WeakAuras.CheckRange(tank2, 10, "<=")) then 
+        if(WeakAuras.CheckRange(unit, 10, "<=")) then 
             return "|cff00ff00TRUE|r"            
         else
             return  "|cffff3333FALSE|r"  
