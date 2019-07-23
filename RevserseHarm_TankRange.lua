00eec1a93111a26d5b621e5e30956bd8ef68e393
Matthew Thomas <matthew.thomas@tiani-spirit.com>
2019-07-23 18:00:39 +0100

    removed main as WoW doesn't like it

--- RevserseHarm_TankRange.lua
+++ RevserseHarm_TankRange.lua
@@ -1,22 +1,14 @@
-function main() 
+function() 
     -- change tank1 and tank2 local variables to match main tanks of group. 
     local tank1 = "Daruul"
-    local tank2 = "Ermehdin"
+    local tank2 = "Ermehdin"	
     
     if UnitExists(tank1) or UnitExists(tank2) then        
         
-        if(WeakAuras.CheckRange(tank1, 10, "<=")) then 
-            return "|cff00ff00TRUE|r"
-            
-        elseif (WeakAuras.CheckRange(tank2, 10, "<=")) then
-            return "|cff00ff00TRUE|r"
-            
-        elseif(WeakAuras.CheckRange(tank1, 10, ">")) then
-            return  "|cffff3333FALSE|r"
-            
-        elseif(WeakAuras.CheckRange(tank2, 10, ">")) then
-            return  "|cffff3333FALSE|r"	
-            
+        if(WeakAuras.CheckRange(tank1, 10, "<=") or WeakAuras.CheckRange(tank2, 10, "<=")) then 
+            return "|cff00ff00TRUE|r"            
+        else
+            return  "|cffff3333FALSE|r"  
         end
     end
 end
